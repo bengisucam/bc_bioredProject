f10d314bf868853f2e9ff1d6ebe467d317dcd644
Nur Bengisu ÇAM <nur.cam@tubitak.gov.tr>
2023-07-12 14:09:02 +0300

    Relation class attributes changed. False relation creation is fixed.

--- BC8_Subtask1/data/createData.py
+++ BC8_Subtask1/data/createData.py
@@ -6,13 +6,14 @@ from typing import List
 
 from nltk.tokenize import sent_tokenize
 import scispacy
+
 os.environ['FOR_DISABLE_CONSOLE_CTRL_HANDLER'] = '1'
 
 
 class Relation:
-    def __init__(self, e1, e2, type, is_novel, is_relation, candidate_sentences):
-        self.e1 = e1
-        self.e2 = e2
+    def __init__(self, e1_id, e2_id, type, is_novel, is_relation, candidate_sentences):
+        self.e1_id = e1_id
+        self.e2_id = e2_id
         self.type = type
         self.is_novel = is_novel
         self.is_relation = is_relation
@@ -75,12 +76,11 @@ def addNewEntityAndUpdateNames(entity_dict, document_id, new_entity):
 
 def getAllPossibleRelationPairs(entity_list):
     possible_pairs = []
-    for i in range(len(entity_list)-1):
+    for i in range(len(entity_list) - 1):
         first = entity_list[i]
-        for j in range(i+1, len(entity_list)):
+        for j in range(i + 1, len(entity_list)):
             second = entity_list[j]
-            if first.id != second.id:
-                possible_pairs.append([first, second])
+            possible_pairs.append([first, second])
     return possible_pairs
 
 
@@ -129,29 +129,33 @@ def createDocumentEntitiesAndRelations(in_pubtator_path):
             elif line_count > 2 and not isDigit(line_items[1]):
                 relation_type = line_items[1]
                 entity1_id, entity2_id = line_items[2], line_items[3]
-                if entity1_id == entity2_id:
-                    print("same entities cannot occur in a relation")
                 is_relation_novel = True if line_items[-1].strip() == "Novel" else False
-                #create True relation pairs
-                relationObject = Relation(e1=entity1_id, e2=entity2_id, type=relation_type, is_relation=True,
+                # create True relation pairs
+                relationObject = Relation(e1_id=entity1_id, e2_id=entity2_id, type=relation_type, is_relation=True,
                                           is_novel=is_relation_novel, candidate_sentences=None)
                 all_docs[doc_id].relation_pairs.append(relationObject)
+
     # next, create False relation pairs
-    total_true_relations = 0
-    total_relations = 0
     for doc_id in all_entities:
         possible_pairs = getAllPossibleRelationPairs(all_entities[doc_id])
         true_relations = all_docs[doc_id].relation_pairs
-        total_true_relations += len(true_relations)
+        false_relation_list = []
         for pair in possible_pairs:
             entity1, entity2 = pair[0], pair[1]
-            if not [rel for rel in true_relations if rel.e1 == entity1.id and rel.e2 == entity2.id]:
-                false_relation = Relation(e1=entity1.id, e2=entity2.id, is_relation=False, type=None, is_novel=False,
+            if len([rel for rel in true_relations if rel.e1_id == entity1.id and rel.e2_id == entity2.id]) != 0:
+                continue
+            # todo:
+            elif len([rel for rel in true_relations if rel.e1_id == entity2.id and rel.e2_id == entity1.id]) != 0:
+                continue
+            else:
+                false_relation = Relation(e1_id=entity1.id, e2_id=entity2.id, is_relation=False, type=None,
+                                          is_novel=False,
                                           candidate_sentences=None)
-                all_docs[doc_id].relation_pairs.append(false_relation)
-        total_relations += len(all_docs[doc_id].relation_pairs)
+                false_relation_list.append(false_relation)
 
-    return all_docs, all_entities, total_true_relations, total_relations
+        all_docs[doc_id].relation_pairs.extend(false_relation_list)
+
+    return all_docs, all_entities
 
 
 def removeEntitiesWithSameLocsButDifferentIDs(entity_list):
@@ -186,7 +190,8 @@ def normalizeEntityNames(entity_dict, doc_dict):
                     elif e.locs[0] > entity.locs[1]:
                         e.locs[0], e.locs[1] = e.locs[0] + diff, e.locs[1] + diff
             elif diff > 0:
-                raise Exception("yeni entity ismi, eski entity isminden uzun olamaz")
+                continue
+                #raise Exception("yeni entity ismi, eski entity isminden uzun olamaz")
             else:
                 continue
     return doc_dict
@@ -210,6 +215,7 @@ def create_sentence_combination(first_list, second_list):
     else:
         return second_list
 
+
 def calculatePossibleNumberOfEntityPairs(entity_dictionary):
     total = 0
     for docid in entity_dictionary:
@@ -217,6 +223,7 @@ def calculatePossibleNumberOfEntityPairs(entity_dictionary):
         total_doc_entities = len(unique_entity_set)
         total += math.comb(total_doc_entities, 2)
     return total
+
 
 if __name__ == '__main__':
     in_data_dir = 'files'
@@ -230,47 +237,56 @@ if __name__ == '__main__':
     out_train_csv_file = out_data_dir + '\\bc8_train.csv'
     out_dev_csv_file = out_data_dir + '\\bc8_dev.csv'
 
-    # process train data
-    doc_dict, entity_dict, total_num_true_rels, total_num_rels = createDocumentEntitiesAndRelations(in_pubtator_path=in_train_pubtator_file)
-    normalized_doc_dict = normalizeEntityNames(entity_dict=entity_dict, doc_dict=doc_dict)
+    in_pubtator_file_list = [in_train_pubtator_file, in_dev_pubtator_file]
+    out_file_list = [out_train_csv_file, out_dev_csv_file]
+    for i in range(0, len(in_pubtator_file_list)):
 
-    print("Actual Total Number of Relation Pairs: ", calculatePossibleNumberOfEntityPairs(entity_dict))
+        # process train data
+        doc_dict, entity_dict = createDocumentEntitiesAndRelations(in_pubtator_path=in_pubtator_file_list[i])
+        normalized_doc_dict = normalizeEntityNames(entity_dict=entity_dict, doc_dict=doc_dict)
 
-    print("Total Number Of Relation Pairs: ", total_num_rels)
-    print("Total Number Of True Relation Pairs: ", total_num_true_rels)
+        # create out file
+        # out file header and data information
+        headers = ["id", "docid", "isValid", "passage"]
+        data = []
+        total_num_relation_sentences = 0
+        total_num_true_relation_sentences = 0
+        for doc_id in normalized_doc_dict:
+            rel_id = 0
+            entities: list[Entity] = entity_dict[doc_id]
+            doc_as_sentences = tokenize_sentences(normalized_doc_dict[doc_id].text)
+            normalized_doc_dict[doc_id].text = doc_as_sentences
+            relation_pairs = normalized_doc_dict[doc_id].relation_pairs
+            for relation in relation_pairs:
+                if relation.is_relation:
+                    total_num_true_relation_sentences += 1
+                total_num_relation_sentences += 1
+                e1 = [e for e in entities if e.id == relation.e1_id][0]
+                e2 = [e for e in entities if e.id == relation.e2_id][0]
+                sentences_with_both = [s for s in doc_as_sentences if e1.name in s and e2.name in s]
+                # if there exists a intra-sentence pair, then no need to look for the inter-sentence pairs
+                if sentences_with_both:
+                    candidate_sentences = sentences_with_both
+                    # relation.candidate_sentences = sentences_with_both
+                else:
+                    sentences_with_e1 = [s for s in doc_as_sentences if e1.name in s and e2.name not in s]
+                    sentences_with_e2 = [s for s in doc_as_sentences if e2.name in s and e1.name not in s]
+                    candidate_sentences = create_sentence_combination(sentences_with_e1, sentences_with_e2)
+                    # relation.candidate_sentences = candidate_sentences
+                relation.candidate_sentences = candidate_sentences
+                # add the plain relation pair sentences into the out folder
+                data.append([rel_id, doc_id, relation.is_relation, ''.join(relation.candidate_sentences)])
+                rel_id += 1
+                # todo:  find the sdp of the candidate sentences
+        print("Total Relation Sentences: ", total_num_relation_sentences)
+        print("Total True Relation Sentences: ", total_num_true_relation_sentences)
+        with open(out_file_list[i], 'w', encoding='UTF8', newline='') as f:
+            writer = csv.writer(f)
+            # write the header
+            writer.writerow(headers)
+            # write multiple rows
+            writer.writerows(data)
+            f.close()
 
-    # create out file
-    # out file header and data information
-    headers = ["id", "docid", "isValid", "passage"]
-    data = []
-    for doc_id in normalized_doc_dict:
-        rel_id = 0
-        entities: list[Entity] = entity_dict[doc_id]
-        doc_as_sentences = tokenize_sentences(normalized_doc_dict[doc_id].text)
-        normalized_doc_dict[doc_id].text = doc_as_sentences
-        relation_pairs = normalized_doc_dict[doc_id].relation_pairs
-        for relation in relation_pairs:
-            e1 = [e for e in entities if e.id == relation.e1][0]
-            e2 = [e for e in entities if e.id == relation.e2][0]
-            sentences_with_both = [s for s in doc_as_sentences if e1.name in s and e2.name in s]
-            # if there exists a intra-sentence pair, then no need to look for the inter-sentence pairs
-            if sentences_with_both:
-                candidate_sentences = sentences_with_both
-                # relation.candidate_sentences = sentences_with_both
-            else:
-                sentences_with_e1 = [s for s in doc_as_sentences if e1.name in s and e2.name not in s]
-                sentences_with_e2 = [s for s in doc_as_sentences if e2.name in s and e1.name not in s]
-                candidate_sentences = create_sentence_combination(sentences_with_e1, sentences_with_e2)
-                # relation.candidate_sentences = candidate_sentences
-            relation.candidate_sentences = candidate_sentences
-            # add the plain relation pair sentences into the out folder
-            data.append([rel_id, doc_id, relation.is_relation, ''.join(relation.candidate_sentences)])
-            rel_id += 1
-            #todo:  find the sdp of the candidate sentences
-    # with open(out_train_csv_file, 'w', encoding='UTF8', newline='') as f:
-    #     writer = csv.writer(f)
-    #     # write the header
-    #     writer.writerow(headers)
-    #     # write multiple rows
-    #     writer.writerows(data)
-    #     f.close()
+
+
