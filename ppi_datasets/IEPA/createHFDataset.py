70e9287748dd85bf36ec11451a6d021b775e9089
bengisucam <bengisu.cam@gmail.com>
2023-12-18 10:38:30 +0300

    hf file creations

---
+++ ppi_datasets/IEPA/createHFDataset.py
@@ -0,0 +1,32 @@
+
+import pandas as pd
+import json
+from datasets import load_dataset
+from peft import LoraConfig
+import os
+
+def load_and_test(dataset, split):
+    ds = load_dataset(dataset)
+    return ds.head()
+
+if __name__ == '__main__':
+    print(os.getcwd())
+    train_df = pd.read_csv("./out_files_masked/IEPA-train.csv")
+    # val_df = pd.read_csv("LLL_with_INO/out_files_protein_tagged_byhand/LLL-val.csv")
+    test_df = pd.read_csv("./out_files_masked/IEPA-test.csv")
+
+    all_data = {
+    "train" : train_df.to_dict(orient="records"),
+    "test" : test_df.to_dict(orient="records"),
+    #"val" : val_df.to_dict(orient="records")
+    }
+
+    for split in all_data:
+        f_name = "./out_files_masked_hf/" + split + ".jsonl"
+        with open(f_name, "w") as f:
+            for line in all_data[split]:
+                f.write(json.dumps(line) + "\n")
+        f.close()
+
+    # test_head = load_and_test("bengisucam/HPRD50_only_true", split="test")
+    # print(test_head)
